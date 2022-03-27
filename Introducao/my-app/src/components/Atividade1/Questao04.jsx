e51783ffc57fbde992852306aef19864f2b133a3
alehxalves <alehxalves@gmail.com>
2022-03-27 18:21:24 -0300

    Up

---
+++ Introducao/my-app/src/components/Atividade1/Questao04.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Questao02 from "./Questao02";
+
+const Questao03 = () => (
+  <div>
+    <Questao02 nome="Alex A." curso="S.I" cidade="QXD-CE" />
+  </div>
+);
+
+export default Questao03;
